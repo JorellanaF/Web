7663851bed353f255557ae5506ead9af3acd5df8
JorellanaF <00103717@uca.edu.sv>
2018-11-07 22:18:28 -0600

    componentes

--- Api/client/RedSocial/src/app/app.module.ts
+++ Api/client/RedSocial/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavegacionComponent } from './components/navegacion/navegacion.component';
+import { LoginComponent } from './components/user/login/login.component';
+import { ProfileComponent } from './components/user/profile/profile.component';
+import { RegistroComponent } from './components/user/registro/registro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavegacionComponent,
+    LoginComponent,
+    ProfileComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
